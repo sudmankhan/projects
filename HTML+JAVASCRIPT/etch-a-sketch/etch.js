7631cd92b554dbcc28b6713f692c2460ddcb4ea7
Sudman <sudmankhan1293@gmail.com>
2024-08-02 18:51:46 -0400

    finished color change logic with opacity, added size slider

--- HTML+JAVASCRIPT/etch-a-sketch/etch.js
+++ HTML+JAVASCRIPT/etch-a-sketch/etch.js
@@ -1,17 +1,58 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    // row.id = "row";
-    // row.textContent = "row";
-    // container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const item = document.createElement("div");
-        // col.id = "col";
-        // item.textContent = "item";
-        item.style.height = "56px";
-        // item.style.width = "50px";
-        row.appendChild(item);
+const sliderbox = document.getElementById("slidercontainer");
+const slider = document.getElementById("slider");
+slider.style.height = "50px";
+slider.style.width = "400px";
+
+const value = document.getElementById("value");
+value.textContent = slider.value;
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.id = "row"
+        for (let j = 0; j < size; j++) {
+            const item = document.createElement("div");
+            item.id = "item";
+            const boxSize = 890/size;
+            item.style.height = boxSize + "px";
+            item.style.width = boxSize + "px";
+            row.appendChild(item);
+        }
+        container.insertBefore(row, slidecontainer);
     }
-    container.appendChild(row);
 }
+
+createGrid(slider.value);
+
+const items = document.querySelectorAll("#item");
+items.forEach((item) => {item.addEventListener("mouseover", () => {
+    item.style.backgroundColor = "black";
+    if(item.style.opacity < 1.0) item.style.opacity = +item.style.opacity + 0.1;
+});
+});
+
+slider.oninput = () => {
+    console.log(slider.value);
+    value.textContent = slider.value;
+    const rows = document.querySelectorAll("#row");
+    rows.forEach((row) => {
+        row.remove();
+    });
+    createGrid(slider.value);
+    const items = document.querySelectorAll("#item");
+    items.forEach((item) => {item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = "black";
+        if(item.style.opacity < 1.0) item.style.opacity = +item.style.opacity + 0.1;
+    });
+    });
+}
+
+// const button = document.createElement("button");
+// button.style.height = "100px";
+// button.style.width = "100px";
+// container.appendChild(button);
+
+
+// createGrid(16);
