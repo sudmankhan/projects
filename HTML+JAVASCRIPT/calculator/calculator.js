f98229bb35c2b50d02ff32df724a99e1b7a18b32
Sudman <sudmankhan1293@gmail.com>
2024-08-09 16:10:19 -0400

    made a lot of progress: made calc display, added operation functions, added display

--- HTML+JAVASCRIPT/calculator/calculator.js
+++ HTML+JAVASCRIPT/calculator/calculator.js
@@ -1,36 +1,71 @@
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator__keys')
+const add = function(a, b) {
+	return a + b;
+};
 
-keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
-   // Do something
- }
-})
+const subtract = function(a, b) {
+	return a - b;
+};
 
-const key = e.target
-const action = key.dataset.action
+const multiply = function(a, b) {
+  return a * b;
+};
 
-if (!action) {
-    console.log('number key!')
-}
+const divide = function(a, b) {
+  return a/b;
+};
 
-if (
-    action == 'add' ||
-    action == 'subtract' ||
-    action == 'multiply' ||
-    action == 'divide'
-) {
-    console.log('operator key!')
-}
+// const nums = document.querySelectorAll("#num");
+// nums.forEach((item) => {item.addEventListener("click", () => {
+//   console.log("monkey");
+// });
+// });
 
-if (action === 'decimal') {
-    console.log('decimal key!')
-}
+console.log(add(3,4));
+console.log(subtract(3,4));
+console.log(multiply(3,4));
+console.log(divide(12,4));
 
-if (action === 'clear') {
-    console.log('clear key!')
+const display = document.querySelector(".view");
+const nums = document.querySelectorAll(".num");
+nums.forEach((item) => {
+  item.addEventListener("click", () => {
+    if(display.textContent === "0") {
+      display.textContent = item.textContent;
+    } else {
+      display.textContent += item.textContent;
+    }
+    if (display.textContent.length > 14) {
+      display.textContent = display.textContent.slice(0, 14);
+    }
   }
-  
-  if (action === 'calculate') {
-    console.log('equal key!')
+);
+  item.addEventListener("mouseover", () => {item.style.backgroundColor = "rgb(201, 201, 201)";});
+  item.addEventListener("mouseout", () => {item.style.backgroundColor = "rgb(117, 117, 117)";});
+});
+
+
+const funcs = document.querySelectorAll(".func");
+funcs.forEach((item) => {
+  item.addEventListener("click", () => {
+    // clear
+    if (item.id === "clear") display.textContent = "0";
+
+    // negate
+    else if (item.id === "neg") {
+      if(display.textContent !== "0") {
+        if (display.textContent[0] !== "-") display.textContent = "-" + display.textContent;
+        else if (display.textContent[0] === "-") display.textContent = display.textContent.slice(1);
+      }
+    }
+
+    // percent
+    else if (item.id === "percent") {
+      if(display.textContent !== "0") display.textContent = +display.textContent / 100;
+    }
   }
+);
+item.addEventListener("mouseover", () => {item.style.backgroundColor = "rgb(0, 156, 180)";});
+item.addEventListener("mouseout", () => {item.style.backgroundColor = "rgb(0, 110, 126)";});
+});
+
+
